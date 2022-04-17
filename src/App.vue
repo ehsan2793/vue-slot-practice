02b2785c93dc17cf9008e0d1851bd10559b54e73
ehsan2793 <ehsan2793@gmail.com>
2022-04-16 23:46:01 -0700

    change betwenn 2 components

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
   <div>
-   
     <TheHeader />
+    <button @click="setSelectedComponent('ActiveGoals')">Active goals</button>
+    <button @click="setSelectedComponent('MangeGoals')">Mange goals</button>
+    <!-- <ActiveGoals v-if="selectedComponent ==='ActiveGoals' " />
+    <MangeGoals v-else/> -->
+    <component :is="selectedComponent">
 
-</div>
+    </component>
+  </div>
 </template>
 
 <script>
-import TheHeader from './components/TheHeader.vue';  
+import TheHeader from './components/TheHeader.vue';
+import ActiveGoals from './components/ActiveGoal.vue';
+import MangeGoals from './components/MangeGoals.vue';
 // import BadgeList from './components/BadgeList.vue';
 // import UserInfo from './components/UserInfo.vue';
 // import CourseGoals from './components/CourseGoals.vue';
@@ -15,19 +22,28 @@ import TheHeader from './components/TheHeader.vue';
 export default {
   components: {
     TheHeader,
+    ActiveGoals,
+    MangeGoals,
     // BadgeList,
     // UserInfo,
     // CourseGoals,
   },
   data() {
     return {
+      selectedComponent:'ActiveGoals',
       activeUser: {
+
         name: 'Maximilian Schwarzmüller',
         description: 'Site owner and admin',
         role: 'admin',
       },
     };
   },
+  methods:{
+    setSelectedComponent(cmp){
+      this.selectedComponent = cmp
+    }
+  }
 };
 </script>
 
---
+++ src/components/MangeGoals.vue
@@ -0,0 +1,3 @@
+<template>
+    <h2>manage Goal</h2>
+</template>
