a47693bc7322dbb1b2e94a7bd7a687f0b4d64841
ehsan2793 <ehsan2793@gmail.com>
2022-04-16 23:22:19 -0700

    slot added

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+   
+    <TheHeader />
+
+</div>
+</template>
+
+<script>
+import TheHeader from './components/TheHeader.vue';  
+// import BadgeList from './components/BadgeList.vue';
+// import UserInfo from './components/UserInfo.vue';
+// import CourseGoals from './components/CourseGoals.vue';
+
+export default {
+  components: {
+    TheHeader,
+    // BadgeList,
+    // UserInfo,
+    // CourseGoals,
+  },
+  data() {
+    return {
+      activeUser: {
+        name: 'Maximilian Schwarzmüller',
+        description: 'Site owner and admin',
+        role: 'admin',
+      },
+    };
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ src/components/CourseGoals.vue
@@ -0,0 +1,18 @@
+<template>
+    <ul>
+        <li v-for="goal in goals" :key="goal">
+    <slot :goal='goal'/>
+        </li>
+    </ul>
+</template>
+
+
+<script>
+export default {
+    data(){
+        return {
+            goals:['learn' , 'learn more']
+        }
+    }
+}
+</script>
