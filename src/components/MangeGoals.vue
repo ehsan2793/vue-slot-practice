580ebc64672b0b3815e6abdd466c45a8d2d6fb97
ehsan2793 <ehsan2793@gmail.com>
2022-04-17 09:26:23 -0700

    teleport diaglog

--- src/components/MangeGoals.vue
+++ src/components/MangeGoals.vue
@@ -1,6 +1,39 @@
 <template>
-  <div>
-    <h2>manage Goal</h2>
-    <input type="text" />
-  </div>
+  <h2>manage Goal</h2>
+  <input type="text" ref="goal" />
+  <button @click="setGoal">Set Goal</button>
+  <teleport to="body">
+    <ErrorAlert v-if="inputisValid">
+      <h1>input can not be empty</h1>
+      <p>write something so the error goes away</p>
+      <button @click="confirmError">okay</button>
+    </ErrorAlert>
+  </teleport>
 </template>
+
+<script>
+import ErrorAlert from './ErrorAlert.vue';
+
+export default {
+  components: {
+    ErrorAlert,
+  },
+
+  data() {
+    return {
+      inputisValid: false,
+    };
+  },
+  methods: {
+    setGoal() {
+      const enterdValue = this.$refs.goal.value;
+      if (!enterdValue) {
+        this.inputisValid = true;
+      }
+    },
+    confirmError() {
+      this.inputisValid = false;
+    },
+  },
+};
+</script>
